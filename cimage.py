397f1e1d0ea71f0cf6aa571bd55339428d802d61
MannuVilasara <mannuvilasara@gmail.com>
2024-05-01 22:41:00 +0530

    fix: syntax error escape characters

--- cimage.py
+++ cimage.py
@@ -2,117 +2,137 @@ import os
 from os.path import expanduser
 from PIL import Image
 import __main__
+
 output = []
 
 file = __main__.passfile
 
-img = Image.open(file+'.png')
+img = Image.open(file + ".png")
 width, height = img.size
 
+
 def save(inputt):
-  filee = str(expanduser("~"))+"/.cache/mfetch/currentlogo"
-  fp = open(filee, 'w')
-  fp.write(str(inputt))
-  fp.close()
-  
+    filee = str(expanduser("~")) + "/.cache/mfetch/currentlogo"
+    fp = open(filee, "w")
+    fp.write(str(inputt))
+    fp.close()
+
 
 def colconv(col):
-  r, g, b, a = col
-  r = int((r/255)+0.5) != 0
-  g = int((g/255)+0.5) != 0
-  b = int((b/255)+0.5) != 0
-  a = int((a/255)+0.5) != 0
-  
-  if a:
-    if r & g & b:
-      return '7'
-    
-    if g & b:
-      return '6'
-    
-    if r & b:
-      return '5'
-    
-    if r & g:
-      return '3'
+    r, g, b, a = col
+    r = int((r / 255) + 0.5) != 0
+    g = int((g / 255) + 0.5) != 0
+    b = int((b / 255) + 0.5) != 0
+    a = int((a / 255) + 0.5) != 0
 
-    if r:
-      return '1'
-  
-    if g:
-      return '2'
-    
-    if b:
-      return '4'
-    
+    if a:
+        if r & g & b:
+            return "7"
+
+        if g & b:
+            return "6"
+
+        if r & b:
+            return "5"
+
+        if r & g:
+            return "3"
+
+        if r:
+            return "1"
+
+        if g:
+            return "2"
+
+        if b:
+            return "4"
+
+        else:
+            return "0"
     else:
-      return '0'
-  else:
-    return ' '
+        return " "
 
 
-image = ''
+image = ""
 
 for y in range(height):
-  for x in range(width):
-    image = image+str(colconv(img.getpixel((x, y))))
-  image = image+str('\n')
+    for x in range(width):
+        image = image + str(colconv(img.getpixel((x, y))))
+    image = image + str("\n")
 
 ylength = 0
-for line in image.split('\n'):
-  if len(line) > ylength:
-    ylength = len(line)
+for line in image.split("\n"):
+    if len(line) > ylength:
+        ylength = len(line)
 
 imagemod = []
-for line in image.split('\n'):
-  imagemod.append(line+str(' '*(ylength-len(line))))
-image = ('\n'.join(imagemod))
+for line in image.split("\n"):
+    imagemod.append(line + str(" " * (ylength - len(line))))
+image = "\n".join(imagemod)
 
-if not len(image.split('\n'))/2 == int(len(image.split('\n'))/2):
-  wdth = (len(image.split('\n')[0]))
-  image = image+('\n'+(' '*wdth))
-img = image.split('\n')
+if not len(image.split("\n")) / 2 == int(len(image.split("\n")) / 2):
+    wdth = len(image.split("\n")[0])
+    image = image + ("\n" + (" " * wdth))
+img = image.split("\n")
 ylen = len(img)
 
 
 ech = 'echo -e "'
-end = '\e[0m"'
-nul = '\e[0m'
+end = '\\e[0m"'
+nul = "\\e[0m"
 
 
+cols = [
+    "\\e[30m",
+    "\\e[40m",
+    "\\e[31m",
+    "\\e[41m",
+    "\\e[32m",
+    "\\e[42m",
+    "\\e[33m",
+    "\\e[43m",
+    "\\e[34m",
+    "\\e[44m",
+    "\\e[35m",
+    "\\e[45m",
+    "\\e[36m",
+    "\\e[46m",
+    "\\e[37m",
+    "\\e[47m",
+]
 
-cols = ['\e[30m','\e[40m','\e[31m','\e[41m','\e[32m','\e[42m','\e[33m','\e[43m','\e[34m','\e[44m','\e[35m','\e[45m','\e[36m','\e[46m','\e[37m','\e[47m']
 
+def col(col, mod):
+    return cols[col * 2 + mod]
 
-def col(col,mod):
-  return cols[col*2+mod]
 
 def blockrender(val0, val1):
-  out = ''
-  if val0 == ' ' and val1 == ' ':
-    out = ' '
-  else:
-    if val0 != ' ':
-      if val1 != ' ':
-        col0 = col(int(val0),0)
-        col1 = col(int(val1),1)
-      else:
-        col0 = col(int(val0),0)
-        col1 = ''
-      out = col0+col1+'▀'
+    out = ""
+    if val0 == " " and val1 == " ":
+        out = " "
     else:
-      col0 = nul
-      col1 = col(int(val1),0)
-      out = col0+col1+'▄'
+        if val0 != " ":
+            if val1 != " ":
+                col0 = col(int(val0), 0)
+                col1 = col(int(val1), 1)
+            else:
+                col0 = col(int(val0), 0)
+                col1 = ""
+            out = col0 + col1 + "▀"
+        else:
+            col0 = nul
+            col1 = col(int(val1), 0)
+            out = col0 + col1 + "▄"
 
-  return (out+nul)
+    return out + nul
 
-for y in range(int(ylen/2)):
-  line0 = img[y*2]
-  line1 = img[y*2+1]
-  for x in range(len(line0)):
-    output.append(str(blockrender(line0[x],line1[x])))
-  if y != ylen/2:
-    output.append('\n')
 
-save(str(''.join(output)))
+for y in range(int(ylen / 2)):
+    line0 = img[y * 2]
+    line1 = img[y * 2 + 1]
+    for x in range(len(line0)):
+        output.append(str(blockrender(line0[x], line1[x])))
+    if y != ylen / 2:
+        output.append("\n")
+
+save(str("".join(output)))
--- neofetch.py
+++ neofetch.py
@@ -3,91 +3,96 @@ import socket
 import __main__
 from os.path import expanduser
 
+
 def get(thing):
-  th = str("neofetch"+" "+thing)
-  tmp = (os.popen(th).read().replace("\n", "")).split(": ")[1]
-  return tmp
+    th = str("neofetch" + " " + thing)
+    tmp = (os.popen(th).read().replace("\n", "")).split(": ")[1]
+    return tmp
+
 
 pref = {}
-pref_ = open(str(expanduser("~"))+"/.config/mfetch/options").read().split('\n')
+pref_ = open(str(expanduser("~")) + "/.config/mfetch/options").read().split("\n")
 for pref_itm in pref_:
-  try:
-    var, val = pref_itm.split(' ')
-    pref.update({var: val})
-  except:
-    False
+    try:
+        var, val = pref_itm.split(" ")
+        pref.update({var: val})
+    except:
+        False
 
-colon_padding = int(pref['text_spacer_size'])
-logo_padding = int(pref['logo_padding'])
+colon_padding = int(pref["text_spacer_size"])
+logo_padding = int(pref["logo_padding"])
 
-if not pref['split_symbol'] == "null":
-  split_symb = pref['split_symbol']
+if not pref["split_symbol"] == "null":
+    split_symb = pref["split_symbol"]
 else:
-  split_symb = " "
-  
-big = pref['logo_big']=='True'
-redact_ip = pref['hide_ip']=='True'
+    split_symb = " "
 
-#os.system('python3 cimage.py')
-#passfile = 'colour/colours'
-#exec(open("cimage.py").read())
+big = pref["logo_big"] == "True"
+redact_ip = pref["hide_ip"] == "True"
 
-if not pref['os_logo'] == "null":
-  oslogo = pref['os_logo']
+# os.system('python3 cimage.py')
+# passfile = 'colour/colours'
+# exec(open("cimage.py").read())
+
+if not pref["os_logo"] == "null":
+    oslogo = pref["os_logo"]
 else:
-  oslogo = get("distro").lower().split(" ")[0]
+    oslogo = get("distro").lower().split(" ")[0]
 
 if big:
-  logof = ('logo-big')
+    logof = "logo-big"
 else:
-  logof = ('logo')
-passfile = ('logos/'+oslogo+'/'+logof)
+    logof = "logo"
+passfile = "logos/" + oslogo + "/" + logof
 try:
-  open(str(passfile+'.png'))
+    open(str(passfile + ".png"))
 except:
-  passfile = ('logos/linux/'+logof)
+    passfile = "logos/linux/" + logof
 exec(open("cimage.py").read())
 
-logo = str(open(str(expanduser("~"))+"/.cache/mfetch/currentlogo").read())
-colours = str(open("colour/colours").read().replace('\n', ''))
-  
+logo = str(open(str(expanduser("~")) + "/.cache/mfetch/currentlogo").read())
+colours = str(open("colour/colours").read().replace("\n", ""))
+
+
 def packages():
-  pkgs = []
-  for i in get("packages").split(", "):
-    i = i.replace("(", "").replace(")", "")
-    i = i.split(" ")
-    pkgs.append(str(i[1]+" "+i[0]))
-  return ' & '.join(pkgs)
+    pkgs = []
+    for i in get("packages").split(", "):
+        i = i.replace("(", "").replace(")", "")
+        i = i.split(" ")
+        pkgs.append(str(i[1] + " " + i[0]))
+    return " & ".join(pkgs)
+
 
 def get_system_info():
     system_info = {}
-    system_info['OS'] = get("distro").replace("x86_64", "")
-    
+    system_info["OS"] = get("distro").replace("x86_64", "")
+
     cpu = get("cpu").split(" ")
     if cpu[0] == "Intel":
-      system_info['CPU'] = cpu[1]
+        system_info["CPU"] = cpu[1]
     else:
-      system_info['CPU'] = ' '.join(cpu)
-    
-    system_info['GPU'] = get("gpu")
-    system_info['Pkgs'] = packages()
-    system_info['Memory'] = get("memory")
-    system_info['Kernel'] = get("kernel")
-    
+        system_info["CPU"] = " ".join(cpu)
+
+    system_info["GPU"] = get("gpu")
+    system_info["Pkgs"] = packages()
+    system_info["Memory"] = get("memory")
+    system_info["Kernel"] = get("kernel")
+
     return system_info
-    
+
+
 system_info = get_system_info()
 
-line = ['', 'OS', '', 'Kernel', 'Pkgs', '', 'CPU', 'GPU', 'Memory', '', 'Colours']
+line = ["", "OS", "", "Kernel", "Pkgs", "", "CPU", "GPU", "Memory", "", "Colours"]
 
 dat = {}
-dat_ = open('logos/'+oslogo+'/dat').read().split('\n')
+dat_ = open("logos/" + oslogo + "/dat").read().split("\n")
 for pref_itm in dat_:
-  try:
-    var, val = pref_itm.split(': ')
-    dat.update({var: val})
-  except:
-    False
+    try:
+        var, val = pref_itm.split(": ")
+        dat.update({var: val})
+    except:
+        False
 
 cols = []
 cols.append(dat["col"])
@@ -95,17 +100,17 @@ cols.append(dat["col"])
 out = []
 on = 0
 out.append("")
-for i in logo.split('\n'):
+for i in logo.split("\n"):
     try:
-      col = cols[(on % (len(cols)))]
-      leng = (6)-(len(line[on]))
-      if line[on] == 'Colours':
-        g = colours
-      else:
-        g = '\e[1m'+'\e[3'+col+'m'+str(line[on])+'\e[0m'+split_symb+(' '*(leng+colon_padding))+str(system_info[line[on]])
+        col = cols[(on % (len(cols)))]
+        leng = (6) - (len(line[on]))
+        if line[on] == "Colours":
+            g = colours
+        else:
+            g = (f"\\e[1m\\e[3{col}m{str(line[on])}\\e[0m{split_symb}{(" " * (leng + colon_padding))}{str(system_info[line[on]])}")
     except:
-      g = ' '
+        g = " "
     on += 1
-    out.append(str(i)+' '*logo_padding+str(g))
+    out.append(str(i) + " " * logo_padding + str(g))
 
-os.system("echo -e '"+"\n".join(out)+"'")
+os.system("echo -e '" + "\n".join(out) + "'")
